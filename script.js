7cbcb823b2dbd03ab7ddf3f0ba8cbeab4ec37f9e
g4mrpr0n <102053176+g4mrpr0n@users.noreply.github.com>
2024-03-10 16:55:32 +0200

    stupid mascot fix

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      var mascot = document.getElementById("mascot");
+      mascot.classList.add("active");
+    }, 100); // Delay in milliseconds
+  });
